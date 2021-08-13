64e07972530ed8895363f2a3e1d8e72f8db21a70
dayoungYNC <68107013+dadayoung0@users.noreply.github.com>
2021-08-13 13:44:07 +0900

    Delete forceterminate.py

--- forceterminate.py
+++
@@ -1,5 +0,0 @@
-import os   #리눅스 터미널에 명령을 보내기 위해 OS 모듈을 포함합니다
-
-os.system("screen -S core -X quit")   #roscore 종료
-os.system("screen -S lidar -X quit")    #ydlidar_ros X4.launch 종료
-os.system("screen -S mapping -X quit")    #hector_mapping mapping_default.launch 종료
